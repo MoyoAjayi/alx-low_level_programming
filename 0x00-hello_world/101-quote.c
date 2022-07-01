c999b46d78b23c3cd33b3d9b3174f5f4de00e073
Moyo Ajayi <ajayimoyosoore@gmail.com>
2022-06-30 21:59:07 -0700

    low level programming task

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,9 +7,10 @@
  */
 
 int main(void)
+
 {
 
-		write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 5);
+		write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
 
 			return (1);
 
