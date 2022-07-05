a8b089a0f56bd90f01838edb98bfc021e80518c0
Moyo Ajayi <ajayimoyosoore@gmail.com>
2022-07-04 17:30:16 -0700

    low level programming project2

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+*main -> prints the lowercase alphabet in reverse
+*followed by a new line
+*Return: always 0
+*/
+
+int main(void)
+
+{
+		char ch;
+
+		for (ch = 'z'; ch >= 'a'; ch--)
+		{
+			putchar(ch);
+		}
+
+		putchar(10);
+
+		return (0);
+}
