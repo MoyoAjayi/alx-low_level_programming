fa47dc14354a1aea36c7d6ff8f0c95cdb19b7a2c
Moyo Ajayi <ajayimoyosoore@gmail.com>
2022-07-20 17:43:14 -0700

    task0

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -2,17 +2,19 @@
 
 /**
 * _puts_recursion -> function prints a string
-* @s: the char value
+* @s: string parameter
 * Return: the 0 value
 */
 
-void _puts_recursion(char *s);
+void _puts_recursion(char *s)
 {
-	if (*s)
+	if (*s == '\0')
 	{
-		_putchar(*s);
-		_puts_recursion(s + 1);
-	}
-	else
 		_putchar('\n');
+		return;
+	}
+	_putchar(*s);
+	s++;
+	_puts_recursion(s);
 }
+
