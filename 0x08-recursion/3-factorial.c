0792af52d647c8bf0c4b699913c990955ca3289d
Moyo Ajayi <ajayimoyosoore@gmail.com>
2022-07-20 18:34:24 -0700

    task3

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -6,13 +6,21 @@
 * Return: the value factorial
 */
 
-int factorial(int n);
+int factorial(int n)
 {
 	int fact = n;
 
 	if (n < 0)
+
 		return (-1);
-	if (n <= 1)
+
+	else if (n >= 0 && n <= 1)
+
 		return (1);
-	return (n * factorial(n - 1));
+
+	else
+
+		fact *= factorial(n - 1);
+
+	return (fact);
 }
